5f8baeab9582458769342928cd3197f27d965c4b
Richard Allen <richard2891@gmail.com>
2019-05-24 19:42:54 -0400

    add postgresql prompt and ports or pkg prompt

--- fusionpbx_setup.sh
+++ fusionpbx_setup.sh
@@ -3,9 +3,73 @@ if [ `whoami` != root ]; then
   echo "Please run as root."
   exit 1
 fi
-MY_SERVER_NAME="192.168.6.100"
+MY_SERVER_NAME="192.168.1.16"
+DB_PASSWORD=password123
+PORTS=false
 
-pkg install --yes fusionpbx
+header() {
+  HEADER=$1
+  STRLENGTH=$(echo -n $HEADER | wc -m)
+  DISPLAY="  " #65
+  center=`expr $STRLENGTH / 2`
+  max=`expr 33 - $center`
+  echo $max
+  for i in $(seq 1 $max)
+  do
+    DISPLAY="${DISPLAY}-"    
+  done
+  DISPLAY="${DISPLAY} "$HEADER" "
+  
+  STRLENGTH=$(echo -n $DISPLAY | wc -m)
+  max=`expr 65 - $STRLENGTH`
+  for i in $(seq 1 $max)
+  do
+    DISPLAY="${DISPLAY}-"
+  done
+    
+  clear
+  echo "  =================================================================="
+  echo "$DISPLAY"
+  echo "  =================================================================="
+  echo ""
+}
+
+question() {
+  HEADER=$1
+  QUESTION=$2
+  RTN=0
+  
+  header "$HEADER"
+  echo "    $QUESTION? [Y/N]"
+  
+  read yesno
+  
+  case $yesno in
+    [Yy]* ) RTN=1;;
+    [Nn]* ) RTN=0;;
+  esac
+  
+  return $RTN
+}
+
+install_sw() {
+  PACKAGE=$1
+  DIR="$2/$PACKAGE"
+  if [ "$PORTS" = true ]; then
+    cd $DIR
+    make deinstall
+    make -DBATCH install clean
+  else
+    pkg install --yes $PACKAGE
+  fi
+}
+
+question "use ports." "would you like to use ports over pkg"
+if [ "$?" = 1 ]; then
+  PORTS=true
+fi
+
+install_sw fusionpbx /usr/ports/www
 
 sysrc memcached_enable="YES"
 sysrc freeswitch_enable="YES"
@@ -15,7 +79,36 @@ sysrc freeswitch_group="freeswitch"
 
 service freeswitch restart
 
-pkg install --yes apache24 mod_php72 
+install_sw apache24 /usr/ports/www
+install_sw mod_php72 /usr/ports/www
+
+question "postgresql install." "would you like to install postgresql10-server"
+if [ "$?" = 1 ]; then
+  install_sw postgresql10-server /usr/ports/databases
+  
+  service postgresql start
+
+  sysrc postgresql_enable="YES"
+  
+  /usr/local/etc/rc.d/postgresql initdb
+
+  sudo -u postgres /usr/local/bin/pg_ctl -D /var/db/postgres/data10 -l logfile start
+
+  service postgresql restart
+  
+  sudo -u postgres psql -c "DROP DATABASE fusionpbx;"
+  sudo -u postgres psql -c "DROP DATABASE freeswitch;"
+  sudo -u postgres psql -c "DROP ROLE fusionpbx;"
+  sudo -u postgres psql -c "DROP ROLE freeswitch;"
+  
+  sudo -u postgres psql -c "CREATE DATABASE fusionpbx;"
+  sudo -u postgres psql -c "CREATE DATABASE freeswitch;"
+  sudo -u postgres psql -c "CREATE ROLE fusionpbx WITH SUPERUSER LOGIN PASSWORD '$DB_PASSWORD';"
+  sudo -u postgres psql -c "CREATE ROLE freeswitch WITH SUPERUSER LOGIN PASSWORD '$DB_PASSWORD';"
+  sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE fusionpbx to fusionpbx;"
+  sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE freeswitch to fusionpbx;"
+  sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE freeswitch to freeswitch;"  
+fi
 
 service freeswitch stop
 

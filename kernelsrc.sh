7a3eec1e0ef931b826ffd5130f6923d414832eb9
Richard Allen <richard2891@gmail.com>
2019-06-05 23:41:10 -0400

    change from snapshots to releases by default. use -s to change to snapshots

--- kernelsrc.sh
+++ kernelsrc.sh
@@ -3,6 +3,15 @@ if [ `whoami` != root ]; then
   echo "Please run as root"
   exit 1
 fi
-fetch -o /tmp ftp://ftp.freebsd.org/pub/`uname -s`/snapshots/`uname -m`/`uname -r | cut -d'-' -f1,2`/src.txz
+if [ "$1" = "-h" ]; then 
+  echo "-s to use snapshots instead of releases"
+  exit 1
+fi
+TRAIN="releases"
+if [ "$1" = "-s" ]; then
+  TRAIN="snapshots"
+fi
+
+fetch -o /tmp ftp://ftp.freebsd.org/pub/`uname -s`/$TRAIN/`uname -m`/`uname -r | cut -d'-' -f1,2`/src.txz
 
 tar -C / -xvf /tmp/src.txz
